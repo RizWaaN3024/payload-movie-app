1f714a67e009bdf8428e281d1d6e8b35a50b6640
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-17 02:23:56 +0530

    Updated movies collection

--- src/app/collections/movie.ts
+++ src/app/collections/movie.ts
@@ -1,5 +1,26 @@
 import { CollectionConfig, FieldHook } from "payload";
 
+const format = (val: string): string =>
+    val
+        .replace(/ /g, '-')
+        .replace(/[^\w-/]+/g, '')
+        .toLowerCase()
+
+const formatSlug =
+    (fallback: string): FieldHook =>
+        ({ value, originalDoc, data }) => {
+            if (typeof value === 'string') {
+                return format(value)
+            }
+            const fallbackData = data?.[fallback] || originalDoc?.[fallback]
+
+            if (fallbackData && typeof fallbackData === 'string') {
+                return format(fallbackData)
+            }
+
+            return value
+        }
+
 export const MoviesCollection: CollectionConfig = {
     slug: 'movies',
     admin: {
@@ -24,6 +45,44 @@ export const MoviesCollection: CollectionConfig = {
             name: 'votes',
             type: 'number',
             required: true
+        },
+        {
+            name: 'poster',
+            type: 'upload',
+            relationTo: 'media', //required
+            required: true
+        },
+        {
+            name: 'overview',
+            type: 'text',
+            required: true
+        },
+        {
+            name: 'tagline',
+            type: 'text',
+            required: true
+        },
+        {
+            name: 'genres',
+            type: 'array',
+            fields: [
+                {
+                    name: 'name',
+                    type: 'text',
+                }
+            ],
+            required: true
+        },
+        {
+            name: 'slug',
+            label: 'slug',
+            type: 'text',
+            admin: {
+                position: 'sidebar',
+            },
+            hooks: {
+                beforeValidate: [formatSlug('name')]
+            }
         }
     ]
 }
