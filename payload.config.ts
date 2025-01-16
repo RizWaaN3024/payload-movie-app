8d3084ef65e0706108312b065ef6c1a237254efd
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-17 02:41:53 +0530

    Update migration and movies collection

--- payload.config.ts
+++ payload.config.ts
@@ -1,25 +1,11 @@
-import path from 'path'
+import { MoviesCollection } from '@/app/collections/movie'
 import { postgresAdapter } from '@payloadcms/db-postgres'
-import { en } from 'payload/i18n/en'
 import {
-  AlignFeature,
-  BlockquoteFeature,
-  BlocksFeature,
-  BoldFeature,
-  ChecklistFeature,
-  HeadingFeature,
-  IndentFeature,
-  InlineCodeFeature,
-  ItalicFeature,
-  lexicalEditor,
-  LinkFeature,
-  OrderedListFeature,
-  ParagraphFeature,
-  RelationshipFeature,
-  UnorderedListFeature,
-  UploadFeature,
+    lexicalEditor
 } from '@payloadcms/richtext-lexical'
+import path from 'path'
 import { buildConfig } from 'payload'
+import { en } from 'payload/i18n/en'
 import sharp from 'sharp'
 import { fileURLToPath } from 'url'
 
@@ -27,91 +13,76 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-  //editor: slateEditor({}),
-  editor: lexicalEditor(),
-  collections: [
-    {
-      slug: 'users',
-      auth: true,
-      access: {
-        delete: () => false,
-        update: () => false,
-      },
-      fields: [],
-    },
-    {
-      slug: 'pages',
-      admin: {
-        useAsTitle: 'title',
-      },
-      fields: [
+    //editor: slateEditor({}),
+    editor: lexicalEditor(),
+    collections: [
         {
-          name: 'title',
-          type: 'text',
+            slug: 'users',
+            auth: true,
+            access: {
+                delete: () => false,
+                update: () => false,
+            },
+            fields: [],
         },
+        MoviesCollection,
         {
-          name: 'content',
-          type: 'richText',
+            slug: 'media',
+            upload: true,
+            fields: [
+                {
+                    name: 'text',
+                    type: 'text',
+                },
+            ],
         },
-      ],
+    ],
+    secret: process.env.PAYLOAD_SECRET || '',
+    typescript: {
+        outputFile: path.resolve(dirname, 'payload-types.ts'),
     },
-    {
-      slug: 'media',
-      upload: true,
-      fields: [
-        {
-          name: 'text',
-          type: 'text',
+    db: postgresAdapter({
+        pool: {
+            connectionString: process.env.POSTGRES_URL || ''
+        }
+    }),
+
+    /**
+     * Payload can now accept specific translations from 'payload/i18n/en'
+     * This is completely optional and will default to English if not provided
+     */
+    i18n: {
+        supportedLanguages: { en },
+    },
+
+    admin: {
+        autoLogin: {
+            email: 'dev@payloadcms.com',
+            password: 'test',
+            prefillOnly: true,
         },
-      ],
     },
-  ],
-  secret: process.env.PAYLOAD_SECRET || '',
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
-  },
-  db: postgresAdapter({
-    pool: {
-      connectionString: process.env.POSTGRES_URL || ''
-    }
-  }),
+    async onInit(payload) {
+        const existingUsers = await payload.find({
+            collection: 'users',
+            limit: 1,
+        })
 
-  /**
-   * Payload can now accept specific translations from 'payload/i18n/en'
-   * This is completely optional and will default to English if not provided
-   */
-  i18n: {
-    supportedLanguages: { en },
-  },
-
-  admin: {
-    autoLogin: {
-      email: 'dev@payloadcms.com',
-      password: 'test',
-      prefillOnly: true,
+        if (existingUsers.docs.length === 0) {
+            await payload.create({
+                collection: 'users',
+                data: {
+                    email: 'dev@payloadcms.com',
+                    password: 'test',
+                },
+            })
+        }
     },
-  },
-  async onInit(payload) {
-    const existingUsers = await payload.find({
-      collection: 'users',
-      limit: 1,
-    })
+    // Sharp is now an optional dependency -
+    // if you want to resize images, crop, set focal point, etc.
+    // make sure to install it and pass it to the config.
 
-    if (existingUsers.docs.length === 0) {
-      await payload.create({
-        collection: 'users',
-        data: {
-          email: 'dev@payloadcms.com',
-          password: 'test',
-        },
-      })
-    }
-  },
-  // Sharp is now an optional dependency -
-  // if you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
-
-  // This is temporary - we may make an adapter pattern
-  // for this before reaching 3.0 stable
-  sharp,
+    // This is temporary - we may make an adapter pattern
+    // for this before reaching 3.0 stable
+    sharp,
 })
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250109_204135_initial from './20250109_204135_initial';
+import * as migration_20250116_210359_movies from './20250116_210359_movies';
 
 export const migrations = [
   {
     up: migration_20250109_204135_initial.up,
     down: migration_20250109_204135_initial.down,
-    name: '20250109_204135_initial'
+    name: '20250109_204135_initial',
+  },
+  {
+    up: migration_20250116_210359_movies.up,
+    down: migration_20250116_210359_movies.down,
+    name: '20250116_210359_movies'
   },
 ];
