8624639c182c9ea59497428949043fbc49707301
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-23 00:40:50 +0530

    Added search api

--- src/movies/index.ts
+++ src/movies/index.ts
@@ -5,4 +5,9 @@ export async function searchMovies(query: string) {
         `http://www.omdbapi.com/?i=tt3896198&apikey=bd9d39a7`
     )
     const { results } = await response.json();
+    return results.map(({ id, poster_path, title }: { id: string; poster_path: string; title: string }) => ({
+        id,
+        poster_path,
+        title
+    })).filter(({ poster_path }: { poster_path: string }) => !poster_path);
 }
