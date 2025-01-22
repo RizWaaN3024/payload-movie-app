7646529707b6f61e66596a6216eae1805a406197
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-23 01:07:40 +0530

    Added Add Movie Page

--- src/app/(app)/Header.tsx
+++ src/app/(app)/Header.tsx
@@ -1,20 +1,17 @@
+import { FilmIcon } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react';
-import { FilmIcon } from 'lucide-react';
 
-const Header = () => {
+export default function Header() {
     return (
-        <header className="flex flex-row gap-2 py-5 px-5 bg-slate-900 text-white items-center">
+        <header className="flex gap-2 py-5 px-5 bg-slate-900 text-white rounded-b-xl items-center">
             <FilmIcon />
             <Link href="/" className="text-xl font-bold">
-                <h1>Our Favourite Movies</h1>
+                <h1>Our Favorite Movies</h1>
             </Link>{' '}
-
+            |
             <Link href="/add" className="text-xl font-light">
                 Add A Movie
             </Link>
         </header>
     )
 }
-
-export default Header
