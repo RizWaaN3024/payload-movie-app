5dff9d9285d16316eec0cb58c5aac536bc01ed2d
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-23 00:58:01 +0530

    Code Fixes

--- src/movies/index.ts
+++ src/movies/index.ts
@@ -1,9 +1,7 @@
-
+'use server';
 
 export async function searchMovies(query: string) {
-    const response = await fetch(
-        `http://www.omdbapi.com/?i=tt3896198&apikey=bd9d39a7`
-    )
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1&api_key=${process.env.TMDB_API_KEY}`);
     const { results } = await response.json();
     return results.map(({ id, poster_path, title }: { id: string; poster_path: string; title: string }) => ({
         id,
