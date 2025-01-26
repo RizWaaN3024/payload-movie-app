ad46a77daa44aba5fdd6c937037815eaa512b4c4
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-26 19:50:15 +0530

    Code Fixes

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,26 +1,24 @@
-import React from 'react'
-
-import { getPayloadHMR } from '@payloadcms/next/utilities'
-import configPromise from '@payload-config'
-
-// import MovieCards from './MovieCards'
+import React from "react";
+import { getPayloadHMR } from "@payloadcms/next/utilities";
+import configPromise from '@payload-config';
+import MovieCards from "../MovieCards";
 
 
 const Page = async () => {
-    const payload = await getPayloadHMR({ config: configPromise })
+    const payload = await getPayloadHMR({ config: configPromise });
 
     const movies = await payload.find({
         collection: 'movies',
-        sort: '-votes',
+        sort: '-votes'
     })
 
     return (
         <>
             <main className="mt-5">
-                {/* <MovieCards movies={movies.docs} /> */}
+                <MovieCards movies={movies.docs} />
             </main>
         </>
     )
 }
 
-export default Page
+export default Page;
